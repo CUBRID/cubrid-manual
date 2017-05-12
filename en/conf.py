3d84937e42c790445ca2b1498387454a5157a4f4
Remzi Ibram <remzi.ibram@arnia.ro>
2017-05-12 12:41:37 +0300

    a better way of add custom style.css

--- en/conf.py
+++ en/conf.py
@@ -158,6 +158,9 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+def setup(app):
+  app.add_stylesheet('style.css')
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'

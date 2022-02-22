bb2272441ef7b9ea268773f20a1028637a813b91
Mike MyungHwan Oh <mhoh@cubrid.com>
2022-02-22 09:51:35 +0900

    Update en/conf.py
    Co-authored-by: Hyung-Gyu Ryoo <hgryoo@cubrid.com>

--- en/conf.py
+++ en/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CUBRID'
-copyright = u'2021, CUBRID Corporation'
+copyright = u'2016, CUBRID Corporation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

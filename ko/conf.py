f7d9e94c4391190128cd42e3b4c70110029dd620
hiclass <hiclasss@naver.com>
2025-08-08 08:42:59 +0000

    modify build number conf.py

--- ko/conf.py
+++ ko/conf.py
@@ -37,8 +37,8 @@ master_doc = 'index'
 project = u'CUBRID'
 copyright = u'2016, CUBRID Corparation'
 
-version = '10.0'
-release = '10.0.0'
+version = '11.2'
+release = '11.2.0'
 
 html_context = {
     "switcher_url": "https://ftp.cubrid.org/CUBRID_Docs/Manuals/switcher.json"
